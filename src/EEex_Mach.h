f976e6d6530c40060a7b140eea9088bb31033121
CrevsDaak <crevs.daak@gmail.com>
2019-07-13 23:04:18 -0300

    uuh, forgot this before

--- src/EEex_Mach.h
+++ src/EEex_Mach.h
@@ -2,15 +2,7 @@
 #define __EEEX_MACH_H__
 
 int EEex_protect(void*, vm_prot_t, bool);
-
-extern inline int EEex_write_byte(void*, int8_t);
-extern inline int EEex_write_word(void*, int16_t);
-extern inline int EEex_write_dword(void*, int32_t);
-extern inline int EEex_write_qword(void*, int64_t);
-
-extern inline int EEex_read_byte(void*, int8_t);
-extern inline int EEex_read_word(void*, int16_t);
-extern inline int EEex_read_dword(void*, int32_t);
-extern inline int EEex_read_qword(void*, int64_t);
+int EEex_write(void*, void*, uint32_t);
+int EEex_read(void*, void*, uint32_t);
 
 #endif /* !__EEEX_MACH_H__ */
