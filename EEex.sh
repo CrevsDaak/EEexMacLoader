a2a75df37af0860bc43f00fa7114f27acaf69ee7
CrevsDaak <crevs.daak@gmail.com>
2019-07-27 00:39:13 -0300

    update EEex Mac WeiDU install lib

--- EEex.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-#TODO: make WeiDU modify this for fucks sake
-cd "/Applications/Baldur's Gate - Enhanced Edition/Game Data/00766/Baldur's Gate - Enhanced Edition.app/Contents/MacOS/"
-DYLD_INSERT_LIBRARIES=EEexLoader.dylib ./Baldur\'s\ Gate\ -\ Enhanced\ Edition; exit;
