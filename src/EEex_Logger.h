b54a09faebc56453db6154c0d3f6a8813d1f53b1
CrevsDaak <crevs.daak@gmail.com>
2019-11-15 20:59:41 -0300

    ah, didn't end up using this, so I'll just delete it don't mind me

--- src/EEex_Logger.h
+++ src/EEex_Logger.h
@@ -1,16 +1,6 @@
 #ifndef __EEEX_LOGGER_H__
 #define __EEEX_LOGGER_H__
 
-enum EEex_log_level
-{
-    EEEX_LOG_DEBUG = 0x1,
-    EEEX_LOG_ERROR = 0x2,
-    EEEX_LOG_GAME_MSG = 0x4,
-    EEEX_LOG_INFO = 0x8,
-    EEEX_LOG_DIALOG = 0x10,
-    EEEX_LOG_PRINT = 0x20
-};
-
 /* from dfhack */
 #define DYLD_INTERPOSE(_replacement, _replacee)                                                              \
     __attribute__((used)) static struct                                                                      \
